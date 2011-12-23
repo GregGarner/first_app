718d9837550a269b7a4015a2ca0ce3132a02063d
Greg <Greg@apple-tv.gateway.2wire.net>
2011-12-23 13:03:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '786c7ceef801f15ae61c451c6c110b590bf28a204aa2d6245c9066eb294e1b89221b5187d70505309e34017e8407ff488b2a024e9a16589844b48ce65b894a9c'
